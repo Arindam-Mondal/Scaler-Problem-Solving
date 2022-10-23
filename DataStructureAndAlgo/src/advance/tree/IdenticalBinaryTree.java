4a8798ef0e01cd7ca2292ea55ae3c5022c25a508
Arindam <ariron123@gmail.com>
2022-10-23 11:43:09 +0530

    Identical Binary Tree

---
+++ DataStructureAndAlgo/src/advance/tree/IdenticalBinaryTree.java
@@ -0,0 +1,79 @@
+package advance.tree;
+
+/**
+ *  Identical Binary Trees
+ *
+ *  Problem Description
+ * Given two binary trees, check if they are equal or not.
+ *
+ * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+ *
+ *
+ *
+ * Problem Constraints
+ * 1 <= number of nodes <= 105
+ *
+ *
+ *
+ * Input Format
+ * The first argument is a root node of the first tree, A.
+ *
+ * The second argument is a root node of the second tree, B.
+ *
+ *
+ *
+ * Output Format
+ * Return 0 / 1 ( 0 for false, 1 for true ) for this problem.
+ *
+ *
+ *
+ * Example Input
+ * Input 1:
+ *
+ *    1       1
+ *   / \     / \
+ *  2   3   2   3
+ * Input 2:
+ *
+ *    1       1
+ *   / \     / \
+ *  2   3   3   3
+ *
+ *
+ * Example Output
+ * Output 1:
+ *
+ *  1
+ * Output 2:
+ *
+ *  0
+ *
+ *
+ * Example Explanation
+ * Explanation 1:
+ *
+ *  Both trees are structurally identical and the nodes have the same value.
+ * Explanation 2:
+ *
+ *  Values of the left child of the root node of the trees are different.
+ */
+public class IdenticalBinaryTree {
+    public int isSameTree(TreeNode A, TreeNode B) {
+        return isIdentical(A,B) ? 1 : 0;
+    }
+
+    public boolean isIdentical(TreeNode A, TreeNode B){
+        if( A==null && B==null){
+            return true;
+        }else if(A==null){
+            return false;
+        }else if(B==null){
+            return false;
+        }else if(A.val != B.val){
+            return false;
+        }
+        boolean left = isIdentical(A.left,B.left);
+        boolean right = isIdentical(A.right,B.right);
+        return left && right;
+    }
+}
